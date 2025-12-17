7edca1595753f68a4aff03c2c0f52da8433c6073
Lin-Yuxia2025 <alfonzo55555@gmail.com>
2025-12-17 13:35:31 +0900

    run-python-test1

--- api/run-python.py
+++ api/run-python.py
@@ -1,24 +1,51 @@
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
+# import sys
+# from pathlib import Path
+
+# app = Flask(__name__)
+
+# # src/python-scriptsからimportできるように
+# root = Path(__file__).resolve().parent.parent
+# py_scripts = root / "src" / "python-scripts"
+# sys.path.append(str(py_scripts))
+
+# # parse.pyから関数parseを取得
+# import parse
+# parse_run = parse.parse
+
+# # vercelで動かす場合、indexでなくファイル名(/api/run-python)を探す
+# # とりあえずここを試す。
+# @app.route("/", methods=["POST"])        # このファイル自体が/api/run-pythonだから"/"
+# def run_python():
+#     state = request.get_json()          # リクエストのJSON文字列をPythonの辞書に変換して取得
+
+#     result = parse_run(state)           # 呼び出し実行
+
+#     return jsonify(result)              # Pythonの辞書をJSON形式のレスポンスに変換して返す
+
+
+
+
+
+
+
+# api/run-python.py
+import json
 import sys
 from pathlib import Path
 
-app = Flask(__name__)
+ROOT = Path(__file__).resolve().parent.parent
+PY_SCRIPTS = ROOT / "src" / "python-scripts"
+sys.path.append(str(PY_SCRIPTS))
 
-# src/python-scriptsからimportできるように
-root = Path(__file__).resolve().parent.parent
-py_scripts = root / "src" / "python-scripts"
-sys.path.append(str(py_scripts))
-
-# parse.pyから関数parseを取得
 import parse
-parse_run = parse.parse
 
-# vercelで動かす場合、indexでなくファイル名(/api/run-python)を探す
-# とりあえずここを試す。
-@app.route("/", methods=["POST"])        # このファイル自体が/api/run-pythonだから"/"
-def run_python():
-    state = request.get_json()          # リクエストのJSON文字列をPythonの辞書に変換して取得
+def handler(request):
+    state = request.json
+    result = parse.parse(state)
+    return {
+        "statusCode": 200,
+        "headers": {"content-type": "application/json; charset=utf-8"},
+        "body": json.dumps(result, ensure_ascii=False),
+    }
 
-    result = parse_run(state)           # 呼び出し実行
-
-    return jsonify(result)              # Pythonの辞書をJSON形式のレスポンスに変換して返す
